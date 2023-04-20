450c1adf852bfb39fb08af238e99488aaca5d932
Anubhav Singh <singhanubhav58@gmail.com>
2023-04-20 20:41:37 +0530

    express-session -> cookie-session

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const session = require('express-session');
+const session = require('cookie-session');
 const dotenv = require('dotenv').config();
 const path = require('path');
 
@@ -12,24 +12,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // database
-const dbURL = process.env.DB_URL || 'mongodb://localhost:27017/online-personal-diary';
-mongoose.connect(dbURL)
-    .then(() => console.log('DB Connected'))
-    .catch((err) => console.log('Cannot connect to DB ', err))
+const dbURL =
+  process.env.DB_URL || 'mongodb://localhost:27017/online-personal-diary';
+mongoose
+  .connect(dbURL)
+  .then(() => console.log('DB Connected'))
+  .catch((err) => console.log('Cannot connect to DB ', err));
 
 // session config
 const sessionConfig = session({
-    secret: process.env.SECRET || 'very complex secret',
-    cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
-    saveUninitialized: true,
-    resave: true
+  secret: process.env.SECRET || 'very complex secret',
+  cookie: { maxAge: 7 * 24 * 60 * 60 * 1000 },
+  saveUninitialized: true,
+  resave: true,
 });
 app.use(sessionConfig);
 
 app.use((req, res, next) => {
-    res.locals.currentUser = req.session.user_id;
-    res.locals.fullname = req.session.fullname;
-    next();
+  res.locals.currentUser = req.session.user_id;
+  res.locals.fullname = req.session.fullname;
+  next();
 });
 
 app.set('view engine', 'ejs');
@@ -40,7 +42,6 @@ app.use(express.static('public'));
 // routes
 app.use('/', userRoutes);
 app.use('/', dairyRoutes);
-
 
 // connect express
 const port = process.env.PORT || 3000;
