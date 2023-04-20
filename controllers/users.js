d8143cabea1e6b2cdbf4852ec413a7b9a215b8da
Anubhav Singh <singhanubhav58@gmail.com>
2023-04-20 20:51:45 +0530

    express-session -> cookie-session

--- controllers/users.js
+++ controllers/users.js
@@ -2,64 +2,66 @@ const User = require('../models/user');
 const Diary = require('../models/diary');
 
 module.exports.register = async (req, res) => {
-    const {
-        fname, username, password, email
-    } = req.body;
+  const { fname, username, password, email } = req.body;
 
-    let isAdmin = false;
-    if (username === 'anubhav' || username === 'admin') {
-        isAdmin = true;
+  let isAdmin = false;
+  if (username === 'anubhav' || username === 'admin') {
+    isAdmin = true;
+  }
+
+  User.find(
+    {
+      username: username,
+    },
+    async (err, docs) => {
+      if (docs.length) {
+        res.render('message', { msgid: 4 });
+      } else {
+        const user = new User({
+          fname,
+          username,
+          password,
+          email,
+          isAdmin,
+        });
+        await user.save();
+
+        Diary.create({ username: username }, (err, docs) => {});
+        req.session.user_id = user._id;
+        req.session.fullname = user.fname;
+        req.session.username = user.username;
+        res.redirect('/');
+      }
     }
-
-    User.find({
-        username: username
-    }, async (err, docs) => {
-        if (docs.length) {
-            res.render("message", { msgid: 4 });
-        } else {
-            const user = new User({
-                fname, username, password, email, isAdmin
-            })
-            await user.save();
-
-            Diary.create({ username: username }, (err, docs) => { });
-            req.session.user_id = user._id;
-            req.session.fullname = user.fname;
-            req.session.username = user.username;
-            res.redirect("/");
-        }
-    })
-}
+  );
+};
 
 module.exports.login = async (req, res) => {
-    const {
-        username,
-        password
-    } = req.body;
-    const foundUser = await User.findAndValidate(username, password)
-    if (foundUser) {
-        req.session.user_id = foundUser._id;
-        req.session.fullname = foundUser.fname;
-        req.session.username = foundUser.username;
-        res.redirect("/");
-    } else {
-        res.render('message', { msgid: 1 });
-    }
-}
+  const { username, password } = req.body;
+  const foundUser = await User.findAndValidate(username, password);
+  if (foundUser) {
+    req.session.user_id = foundUser._id;
+    req.session.fullname = foundUser.fname;
+    req.session.username = foundUser.username;
+    res.redirect('/');
+  } else {
+    res.render('message', { msgid: 1 });
+  }
+};
 
 module.exports.getLogin = (req, res) => {
-    res.render('login');
-}
+  res.render('login');
+};
 
 module.exports.getRegister = (req, res) => {
-    res.render('register');
-}
+  res.render('register');
+};
 
 module.exports.home = (req, res) => {
-    res.render('home');
-}
+  res.render('home');
+};
 
 module.exports.logout = (req, res) => {
-    req.session.destroy();
-    res.redirect("/");
-}
+  req.session = null;
+  res.redirect('/');
+};
